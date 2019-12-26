931c89666cf7f72bbd145f1be725e2b9d5893c70
Andrey Kechin <a.a.kechin@gmail.com>
2019-12-26 19:19:18 +0700

    Add BAT-scripts for installing kivy in Windows

---
+++ install_kivy_step1.bat
@@ -0,0 +1,6 @@
+python -m pip install --upgrade pip wheel setuptools virtualenv
+python -m virtualenv kivy_venv
+kivy_venv\Scripts\activate
+python -m pip install docutils pygments pypiwin32 kivy_deps.sdl2==0.1.* kivy_deps.glew==0.1.*
+python -m pip install kivy_deps.gstreamer==0.1.*
+python -m pip install kivy==1.11.1
---
+++ install_kivy_step2.bat
@@ -0,0 +1,3 @@
+python -m pip install docutils pygments pypiwin32 kivy_deps.sdl2==0.1.* kivy_deps.glew==0.1.*
+python -m pip install kivy_deps.gstreamer==0.1.*
+python -m pip install kivy==1.11.1
