a8e65b2eb0dd5fa3eff12a36124384c4fcd2cffd
Andrey Kechin <a.a.kechin@gmail.com>
2019-12-27 09:55:26 +0700

    Add files for automatic installation

---
+++ install_GUI_linux.sh
@@ -0,0 +1,3 @@
+pip3 install -r linux_GUI_requirements.txt
+python3 -m virtualenv ~/kivy_venv
+pip3 install kivy
---
+++ install_GUI_windows.bat
@@ -0,0 +1,6 @@
+python -m pip install --upgrade pip wheel setuptools virtualenv
+python -m virtualenv kivy_venv
+python -m pip install docutils pygments pypiwin32 kivy_deps.sdl2==0.1.* kivy_deps.glew==0.1.*
+python -m pip install kivy_deps.gstreamer==0.1.*
+python -m pip install kivy==1.11.1
+python -m pip install docker
