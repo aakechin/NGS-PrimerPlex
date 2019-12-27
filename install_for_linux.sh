6ff881baeea088b0011ac60344cfd2fb1345a972
Andrey Kechin <a.a.kechin@gmail.com>
2019-12-27 10:51:44 +0700

    Add other requirements

--- install_for_linux.sh
+++ install_for_linux.sh
@@ -1,2 +1,4 @@
-apt-get install python3 python3-pip
+apt-get update
+apt-get -y install apt-utils python3 python3-pip less vim bwa zlib1g-dev bzip2 libbz2-dev liblzma-dev
+python3 -m pip install --upgrade pip
 python3 -m pip install -r linux_requirements.txt
