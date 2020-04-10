8e978ade68a4538f16f9e67a8a82c41bf5162d27
Andrey Kechin <a.a.kechin@gmail.com>
2020-04-10 21:03:03 +0700

    Improve testing by format, SNPs and non-targets

--- test.py
+++ test.py
@@ -13,9 +13,70 @@
 import os
 import argparse
 import sys
+import xlrd
 import subprocess as sp
 
 thisDir=os.path.dirname(os.path.realpath(__file__))+os.sep
+
+# Section of functions
+def checkExcelFile(fileName,
+                   expectedRowNums,
+                   expectedColNums,
+                   expectedSheetNums=1,
+                   emptyCells=None,
+                   badPrimers=None,
+                   badFilter=None):
+    # Checking the main output file
+    wb=xlrd.open_workbook(fileName)
+    for j in range(wb.nsheets):
+        if j+1>expectedSheetNums:
+            break
+        ws=wb.sheet_by_index(j)
+        if ((type(expectedRowNums[j])==list and
+             ws.nrows not in list(range(expectedRowNums[j][0],
+                                        expectedRowNums[j][-1]+1))) or
+            (type(expectedRowNums[j])==int and
+             ws.nrows!=expectedRowNums[j])):
+            print('Finished successfully '
+                  'but the output contains incorrect number of rows:')
+            print(fileName)
+            print('The index of the worksheet:',j+1)
+            print('The expected number of rows:',expectedRowNums[j])
+            print('The obtained number of rows:',ws.nrows)
+            exit(11)
+        for i in range(ws.nrows):
+            row=ws.row_values(i)
+            if len(row)<expectedColNums[j]:
+                print('Finished successfully '
+                      'but the output contains incorrect number of columns:')
+                print(fileName)
+                print('The index of the worksheet:',j+1)
+                print('The expected number of columns:',expectedColNums[j])
+                print('The obtained number of columns:',len(row))
+                exit(12)
+            if ('' in row and
+                (emptyCells==None or
+                 row.count('')>1 or
+                 row.index('')!=emptyCells[i])):
+                print('Finished successfully '
+                      'but the output contains empty cells!')
+                print(fileName)
+                print('The index of the worksheet:',j+1)
+                print('The index of row:',i+1)
+                print('The index of cell:',row.index('')+1)
+                print('The expected emppy cells (by rows):',emptyCells)
+                print('Row:',row)
+                exit(13)
+            if (j==0 and
+                badPrimers!=None and
+                badPrimers==row[0]):
+                print('Finished successfully '
+                      'but the output contains primers that should have been filtered out!')
+                print(fileName)
+                print('The index of the worksheet:',j+1)
+                print('The index of row:',i+1)
+                print('Primers that should have been filtered out due to '+badFilter+':',badPrimers)
+                exit(14)
 
 # Section of reading arguments
 parser=argparse.ArgumentParser(description='This script performs some tests of NGS-PrimerPlex')
@@ -36,7 +97,7 @@ parser.add_argument('--dbsnp-vcf','-dbsnp',
                  required=False)
 args=parser.parse_args()
 
-print('\n### Global testing of almost all functions of NGS-PrimerPlex started (11 tests)! ###')
+print('\n### Global testing of almost all functions of NGS-PrimerPlex started (9 tests)! ###')
 
 print('\n# 1. Testing extraction of genome regions... #')
 cmd=['python3',thisDir+'getGeneRegions.py',
@@ -59,6 +120,20 @@ out=[]
 for c in iter(process.stdout.readline,''):
     out.append(str(c))
 if 'NGS-PrimerPlex finished!' in '\n'.join(out):
+    # Checking output
+    file=open(thisDir+os.sep.join(['test',
+                                   'test_gene.regions.csv']))
+    expectedCols=[['KRAS','1','B','NotW'],
+                  ['EGFR','1','B','NotW'],
+                  ['EGFR','1','B','NotW']]
+    i=0
+    for string in file:
+        cols=string.replace('\n','').split('\t')
+        if cols[3:]!=expectedCols[i]:
+            print('Finished successfully '
+                  'but the output differs from the expected one!')
+            exit(10)
+        i+=1
     print('Finished successfully!')
 else:
     print('ERROR! This test was finished with error!')
@@ -73,7 +148,11 @@ cmd=['python3',thisDir+'NGS_primerplex.py',
      '-blast','-snps',
      '-ad1','ctctctatgggcagtcggtgatt',
      '-ad2','ctgcgtgtctccgactcag',
-     '-returnvariantsnum','1']
+     '-returnvariantsnum','1',
+     '-primernum1','5',
+     '-minampllen','145',
+     '-optampllen','150',
+     '-maxampllen','150']
 if args.wholeGenomeRef:
     cmd.extend(['-ref',args.wholeGenomeRef])
 else:
@@ -89,6 +168,24 @@ out=[]
 for c in iter(process.stdout.readline,''):
     out.append(str(c))
 if 'NGS-PrimerPlex finished!' in '\n'.join(out):
+    # Checking the main output file
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_test_primers_combination_1_info.xls']),
+                   [3],[18])
+    # Checking file with draft primers
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_all_draft_primers.xls']),
+                   [[30,40]],[10])
+    if args.wholeGenomeRef:
+        # Checking file with draft primers after blast
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_specificity.xls']),
+                       [[30,40]],[10])
+    if args.dbSnpVcfFile:
+        # Checking file with draft primers after SNPs
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_SNPs.xls']),
+                       [[30,40]],[10])
     print('Finished successfully!')
 else:
     print('ERROR! This test was finished with error!')
@@ -106,17 +203,19 @@ process=sp.Popen(cmd,shell=False,
                  universal_newlines=True)
 out=[]
 for c in iter(process.stdout.readline,''):
-##    sys.stdout.write("\r"+str(c))
     out.append(str(c))
-##    sys.stdout.flush()
 if 'NGS-PrimerPlex finished!' in '\n'.join(out):
+    # Checking output
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_test_primers_combination_1_info.draft.xls']),
+                   [3],[10])
     print('Finished successfully!')
 else:
     print('ERROR! This test was finished with error!')
     print('\n'.join(out))
     exit(3)
 
-print('\n# 4. Testing primer design with draft-primers... #')
+print('\n# 4. Testing primer design with draft-primers, checking for specificity and overlapping SNPs... #')
 cmd=['python3',thisDir+'NGS_primerplex.py',
      '-regions',thisDir+os.sep.join(['test',
                                      'test_gene.regions.csv']),
@@ -125,8 +224,12 @@ cmd=['python3',thisDir+'NGS_primerplex.py',
      '-ad1','ctctctatgggcagtcggtgatt',
      '-ad2','ctgcgtgtctccgactcag',
      '-returnvariantsnum','1',
+     '-primernum1','5',
+     '-minampllen','145',
+     '-optampllen','150',
+     '-maxampllen','150',
      '-draft',thisDir+os.sep.join(['test',
-                                   'test_gene.regions_NGS_primerplex_all_draft_primers.xls'])]
+                                   'draft_primers_for_checking_specificity_and_SNPs.xls'])]
 if args.wholeGenomeRef:
     cmd.extend(['-ref',args.wholeGenomeRef])
 else:
@@ -142,6 +245,28 @@ out=[]
 for c in iter(process.stdout.readline,''):
     out.append(str(c))
 if 'NGS-PrimerPlex finished!' in '\n'.join(out):
+    # Checking the main output file
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_test_primers_combination_1_info.xls']),
+                   [3],[18])
+    # Checking file with draft primers
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_all_draft_primers.xls']),
+                   [35],[10])
+    if args.wholeGenomeRef:
+        # Checking file with draft primers after blast
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_specificity.xls']),
+                       [[34,35]],[10],
+                       badPrimers='CCTCTCCCTCCCTCCAG_TGTGTTCCCGGACATAGTC',
+                       badFilter='non-target hybridizations')
+    if args.dbSnpVcfFile:
+        # Checking file with draft primers after SNPs
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_SNPs.xls']),
+                       [[33,35]],[10],
+                       badPrimers='CTCACCTCCACCGTGCAG_CCCGTATCTCCCTTCCCTGATTA',
+                       badFilter='overlapping SNPs')
     print('Finished successfully!')
 else:
     print('ERROR! This test was finished with error!')
@@ -157,6 +282,8 @@ cmd=['python3',thisDir+'NGS_primerplex.py',
      '-ad1','ctctctatgggcagtcggtgatt',
      '-ad2','ctgcgtgtctccgactcag',
      '-returnvariantsnum','1',
+     '-maxextampllen','170',
+     '-primernum1','3',
      '-embedded']
 if args.wholeGenomeRef:
     cmd.extend(['-ref',args.wholeGenomeRef])
@@ -173,6 +300,24 @@ out=[]
 for c in iter(process.stdout.readline,''):
     out.append(str(c))
 if 'NGS-PrimerPlex finished!' in '\n'.join(out):
+    # Checking the main output file
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_test_primers_combination_1_info.xls']),
+                   [3,3],[18,18],2)
+    # Checking file with draft primers
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_all_draft_primers.xls']),
+                   [[9,13],[9,13]],[10,10],2)
+    if args.wholeGenomeRef:
+        # Checking file with draft primers
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_specificity.xls']),
+                       [[9,13],[9,13]],[10,10],2)
+    if args.dbSnpVcfFile:
+        # Checking file with draft primers
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_SNPs.xls']),
+                       [[9,13],[9,13]],[10,10],2)
     print('Finished successfully!')
 else:
     print('ERROR! This test was finished with error!')
@@ -188,6 +333,8 @@ cmd=['python3',thisDir+'NGS_primerplex.py',
      '-ad1','ctctctatgggcagtcggtgatt',
      '-ad2','ctgcgtgtctccgactcag',
      '-returnvariantsnum','1',
+     '-maxextampllen','170',
+     '-primernum1','3',
      '-embedded',
      '-draft',thisDir+os.sep.join(['test',
                                    'test_gene.regions_NGS_primerplex_all_draft_primers.xls'])]
@@ -206,6 +353,24 @@ out=[]
 for c in iter(process.stdout.readline,''):
     out.append(str(c))
 if 'NGS-PrimerPlex finished!' in '\n'.join(out):
+    # Checking the main output file
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_test_primers_combination_1_info.xls']),
+                   [3,3],[18,18],2)
+    # Checking file with draft primers
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_all_draft_primers.xls']),
+                   [[9,13],[10,19]],[10,10],2)
+    if args.wholeGenomeRef:
+        # Checking file with draft primers
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_specificity.xls']),
+                       [[9,13],[10,19]],[10,10],2)
+    if args.dbSnpVcfFile:
+        # Checking file with draft primers
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_SNPs.xls']),
+                       [[9,13],[10,19]],[10,10],2)
     print('Finished successfully!')
 else:
     print('ERROR! This test was finished with error!')
@@ -221,6 +386,8 @@ cmd=['python3',thisDir+'NGS_primerplex.py',
      '-ad1','ctctctatgggcagtcggtgatt',
      '-ad2','ctgcgtgtctccgactcag',
      '-returnvariantsnum','1',
+     '-maxextampllen','170',
+     '-primernum1','3',
      '-embedded',
      '-primers',thisDir+os.sep.join(['test',
                                      'test_gene.regions_NGS_primerplex_test_primers_combination_1_info.xls'])]
@@ -239,6 +406,24 @@ out=[]
 for c in iter(process.stdout.readline,''):
     out.append(str(c))
 if 'NGS-PrimerPlex finished!' in '\n'.join(out):
+    # Checking the main output file
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_test_primers_combination_1_info.xls']),
+                   [3,3],[18,18],2)
+    # Checking file with draft primers
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_NGS_primerplex_all_draft_primers.xls']),
+                   [[9,13],[9,13]],[10,10],2)
+    if args.wholeGenomeRef:
+        # Checking file with draft primers
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_specificity.xls']),
+                       [[9,13],[9,13]],[10,10],2)
+    if args.dbSnpVcfFile:
+        # Checking file with draft primers
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_NGS_primerplex_all_draft_primers_after_SNPs.xls']),
+                       [[9,13],[9,13]],[10,10],2)
     print('Finished successfully!')
 else:
     print('ERROR! This test was finished with error!')
@@ -249,11 +434,13 @@ print('\n# 8. Testing primer design for embedded anchored PCR... #')
 cmd=['python3',thisDir+'NGS_primerplex.py',
      '-regions',thisDir+os.sep.join(['test',
                                      'test_gene.regions_anchored.csv']),
-     '-th','2','-run','test',
+     '-th','2','-run','test_anchored',
      '-blast','-snps',
      '-ad1','ctctctatgggcagtcggtgatt',
      '-ad2','ctgcgtgtctccgactcag',
      '-returnvariantsnum','1',
+     '-maxextampllen','170',
+     '-primernum1','3',
      '-embedded']
 if args.wholeGenomeRef:
     cmd.extend(['-ref',args.wholeGenomeRef])
@@ -270,6 +457,24 @@ out=[]
 for c in iter(process.stdout.readline,''):
     out.append(str(c))
 if 'NGS-PrimerPlex finished!' in '\n'.join(out):
+    # Checking the main output file
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_anchored_NGS_primerplex_test_anchored_primers_combination_1_info.xls']),
+                   [3,3],[18,18],2,[0,1,2])
+    # Checking file with draft primers
+    checkExcelFile(thisDir+os.sep.join(['test',
+                                        'test_gene.regions_anchored_NGS_primerplex_all_draft_primers.xls']),
+                   [[10,18],[10,18]],[10,10],2)
+    if args.wholeGenomeRef:
+        # Checking file with draft primers
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_anchored_NGS_primerplex_all_draft_primers_after_specificity.xls']),
+                       [[10,18],[10,18]],[10,10],2)
+    if args.dbSnpVcfFile:
+        # Checking file with draft primers
+        checkExcelFile(thisDir+os.sep.join(['test',
+                                            'test_gene.regions_anchored_NGS_primerplex_all_draft_primers_after_SNPs.xls']),
+                       [[10,18],[10,18]],[10,10],2)
     print('Finished successfully!')
 else:
     print('ERROR! This test was finished with error!')
